4826a3715ba1619257e67fe3cdc4655b43a7f9e6
Alexis Tekin <alexteki@amazon.com>
2023-09-14 11:07:15 -0400

    mkdir

--- environment/workshop-set-env.sh
+++ environment/workshop-set-env.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+# Get the current working directory
+CURRENT_DIR=$(pwd)
+
+# Set the subdirectory path
+SUBDIRECTORY="environment"
+
+# Construct the full directory path
+DIR_PATH="$CURRENT_DIR/$SUBDIRECTORY"
+
 # Query AWS account ID
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
 
 # Check if AWS_ACCOUNT_ID is not empty
 if [ -n "$AWS_ACCOUNT_ID" ]; then
-  # Create the .environment-ws.json file
-  cat <<EOF > .environment-ws.json
+  # Create the directory if it doesn't exist
+  mkdir -p "$DIR_PATH"
+
+  # Create the .environment-ws.json file in the specified directory
+  cat <<EOF > "$DIR_PATH/.environment-ws.json"
 {
   "AWS_ACCOUNT_ID": "$AWS_ACCOUNT_ID"
 }
 EOF
-  echo "Created .environment-ws.json with AWS_ACCOUNT_ID: $AWS_ACCOUNT_ID"
+  echo "Created $DIR_PATH/.environment-ws.json with AWS_ACCOUNT_ID: $AWS_ACCOUNT_ID"
 else
-  echo "Failed to retrieve AWS account ID."
+  echo "Failed to retrieve AWS account ID"
 fi
